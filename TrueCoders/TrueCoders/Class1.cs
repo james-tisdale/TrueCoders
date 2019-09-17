821e61cdc1c34bbad71ef6bfaefe87718457e654
James Tisdale <james.tisdale89@gmail.com>
2019-09-17 13:31:05 -0500

    did some work

---
+++ TrueCoders/TrueCoders/Class1.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TrueCoders
+{
+    public class People
+    {
+        public int Age { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string HairColor { get; set; }
+
+    }
+}
---
+++ TrueCoders/TrueCoders/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace TrueCoders
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var james = new People();
+            james.Age = 30;
+            james.FirstName = "James";
+            james.LastName = "Tisdale";
+            james.HairColor = "Blonde";
+
+            var wes = new People();
+            wes.Age = 21;
+            wes.FirstName = "Wesley";
+            wes.LastName = "Hendon";
+            wes.HairColor = "Blonde";
+
+            var nate = new People();
+            nate.Age = 21;
+            nate.FirstName = "Nate";
+            nate.LastName = "McCay";
+            nate.HairColor = "Brown";
+
+            Console.WriteLine($"Name : {james.FirstName} {james.LastName}, Age : {james.Age}, Hair Color: {james.HairColor}.");
+            Console.WriteLine($"Name : {nate.FirstName} {nate.LastName}, Age : {nate.Age}, Hair Color : {nate.HairColor}.");
+            Console.WriteLine($"Name : {wes.FirstName} {wes.LastName}, Age : {wes.Age}, Hair Color : {wes.HairColor}.");
+        }
+    }
+}
